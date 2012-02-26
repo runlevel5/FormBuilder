71a83e0d59164a0420bb2e874166b971265c4929
Trung Lê <joneslee85@gmail.com>
2012-02-27 10:03:09 +1100

    Added platform check

--- config/initializers/rghost.rb
+++ config/initializers/rghost.rb
@@ -1,4 +1,8 @@
 require 'rghost'
 require 'image_size'
 
-RGhost::Config::GS[:path]= 'C:\\Program Files (x86)\\gs\\gs9.05\\bin\\gswin32c.exe'
+if RUBY_PLATFORM.downcase.include?("mswin")
+  RGhost::Config::GS[:path]= 'C:\\Program Files (x86)\\gs\\gs9.05\\bin\\gswin32c.exe'
+else
+  RGhost::Config::GS[:path]= '/usr/local/bin/gs'
+end
